d77dc505c039c3be181f34e94e7d9ed35ce866b6
Viktor Todorov <vikinatora@gmail.com>
2019-08-05 22:57:37 +0300

    added themes

--- _site/scripts/site.js
+++ _site/scripts/site.js
@@ -1,3 +1,11 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     document.getElementsByTagName('body')[0].classList.add('loaded');
+
+    document.getElementById('dark').addEventListener('click', function() {
+        console.log("KUR");
+    })
 });
+
+function changeToWhite() {
+    console.log("KUR");
+}
