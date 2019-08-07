768a63bd869cebc65e7927d2e4623679654298f1
Viktor Todorov <vikinatora@gmail.com>
2019-08-07 20:20:33 +0300

    finally some progress

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ gulp.task("css", function() {
 	return gulp.src( '_assets/scss/**/*.scss' )
 		.pipe( sass().on('error', sass.logError) )
 		.pipe( autoprefixer() )
-		.pipe( gulp.dest( './css/' ) )
+		.pipe( gulp.dest( './docs/css/' ) )
 		.pipe( browserSync.stream({ match: '**/*.css' }) )
 	;
 });
